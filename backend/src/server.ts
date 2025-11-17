af00e87ee35565d79f3f9970c12a1324c03f3570
Krishna <krishna.2024@nst.rishihood.edu.in>
2025-11-17 10:45:11 +0530

    first commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'https://unilodge-self.vercel.app/'
+  'https://unilodge-self.vercel.app'
 ];
 
 app.use(cors({
